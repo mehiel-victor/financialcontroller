7c03bc646558e0869cb4ce378c2e54af8423358e
Victor Mehiel Rocha <victormehiel97@gmail.com>
2024-06-27 01:32:59 -0300

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Financial Controller</Title>
-      <button>Salvar</button>
-    </div>
+      <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+      </>
   );
 }
